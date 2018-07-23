97a431158a6bbee1b2b08ad08314767408c8881a
Calvin Simran Rai <raicalvin@gmail.com>
2018-07-23 05:00:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,14 +2,4 @@ var playlist = {
   artist: song
 };
 
-function updatePlaylist(obj, artist, song) {
-  obj[artist] = song;
-  return playlist;
-}
 
-function removeFromPlaylist(obj, artist) {
-  delete obj.artist;
-  return obj;
-}
-
-console.log(playlist['Selena Gomez'])
