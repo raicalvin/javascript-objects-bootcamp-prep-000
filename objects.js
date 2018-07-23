7e948da979bb5194e38dc917e9155c70003f7b59
Calvin Simran Rai <raicalvin@gmail.com>
2018-07-23 05:17:44 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -9,6 +9,6 @@ function updatePlaylist(obj, artist, song) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName;
+  delete playlist[artistName];
   return playlist;
 }
